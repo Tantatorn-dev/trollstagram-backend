db4fd53e46edfa836d998d6f546dedbf4746b62d
Tantatorn <timefear007@gmail.com>
2019-06-29 13:12:21 +0700

    add haarcascade classifier

--- opencv/image.go
+++ opencv/image.go
@@ -1,1 +1,24 @@
 package opencv
+
+import (
+	"image/color"
+
+	"gocv.io/x/gocv"
+)
+
+var image gocv.Mat
+
+var (
+	blue          = color.RGBA{0, 0, 255, 0}
+	faceAlgorithm = "haarcascade_frontalface_default.xml"
+)
+
+//OpenImage open image.jpg
+func OpenImage() {
+	image = gocv.IMRead("./img/image.jpg", gocv.IMReadUnchanged)
+}
+
+//DetectFace detect a face from image.jpg
+func DetectFace() {
+
+}
--- server.go
+++ server.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"trollstagram-backend/opencv"
 	"trollstagram-backend/route"
 )
 
 func main() {
 	e := route.Init()
+	opencv.OpenImage()
 	e.Logger.Fatal(e.Start(":1323"))
 }
